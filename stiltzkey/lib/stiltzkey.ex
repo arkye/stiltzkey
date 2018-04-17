7963c6e49f7384c362a22a69f8dce5c13a2f4b9f
Jonathan Moraes <arkyebr@gmail.com>
2018-04-17 06:49:14 -0300

    [UI] Materialize at Home

--- stiltzkey/lib/stiltzkey.ex
+++ stiltzkey/lib/stiltzkey.ex
@@ -6,4 +6,11 @@ defmodule Stiltzkey do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  @doc false
+  def contributors do
+    [
+      [name: "Jonathan Henrique Maia de Moraes", github: "@arkye"]
+    ]
+  end
 end
