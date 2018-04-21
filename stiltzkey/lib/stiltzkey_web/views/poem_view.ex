efb380a4fdbaab68a095a04798f92ebb346a584a
Jonathan Moraes <arkyebr@gmail.com>
2018-04-21 04:22:04 -0300

    [UI] Materialize at Poems

--- stiltzkey/lib/stiltzkey_web/views/poem_view.ex
+++ stiltzkey/lib/stiltzkey_web/views/poem_view.ex
@@ -1,6 +1,8 @@
 defmodule StiltzkeyWeb.PoemView do
   use StiltzkeyWeb, :view
 
+  import StiltzkeyWeb.Helpers.Input.Decorator
+
   alias Stiltzkey.Papyrus
 
   def author_username(%Papyrus.Poem{author: author}) do
