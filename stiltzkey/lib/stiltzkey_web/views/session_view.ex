ad37c28f817dbade962adf19850098cf5a1153fb
Jonathan Moraes <arkyebr@gmail.com>
2018-04-21 02:53:58 -0300

    [UI] Materialize at Sessions/Users

--- stiltzkey/lib/stiltzkey_web/views/session_view.ex
+++ stiltzkey/lib/stiltzkey_web/views/session_view.ex
@@ -1,3 +1,6 @@
 defmodule StiltzkeyWeb.SessionView do
   use StiltzkeyWeb, :view
+
+  import StiltzkeyWeb.Helpers.Input.Decorator
+  
 end
--- stiltzkey/lib/stiltzkey_web/views/user_view.ex
+++ stiltzkey/lib/stiltzkey_web/views/user_view.ex
@@ -1,3 +1,6 @@
 defmodule StiltzkeyWeb.UserView do
   use StiltzkeyWeb, :view
+
+  import StiltzkeyWeb.Helpers.Input.Decorator
+
 end
