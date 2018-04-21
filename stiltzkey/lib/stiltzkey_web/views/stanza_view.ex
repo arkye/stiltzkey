13c5b8877f35359f22dfd93475c0457c01f37775
Jonathan Moraes <arkyebr@gmail.com>
2018-04-21 19:29:21 -0300

    [UI] Materialize at Stanzas

--- stiltzkey/lib/stiltzkey_web/views/stanza_view.ex
+++ stiltzkey/lib/stiltzkey_web/views/stanza_view.ex
@@ -1,6 +1,8 @@
 defmodule StiltzkeyWeb.StanzaView do
   use StiltzkeyWeb, :view
 
+  import StiltzkeyWeb.Helpers.Input.Decorator
+
   alias Stiltzkey.Papyrus
 
   def author_username(%Papyrus.Stanza{author: author}) do
